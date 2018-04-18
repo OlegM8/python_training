4806a464bfe34a18092c87dad42184efacc698fc
Oleg Mikheenko <olegmikheenko@Olegs-MacBook-Pro.local>
2018-04-18 19:42:06 +0300

    a lot of shit

--- mail1.py
+++ mail1.py
@@ -10,15 +10,15 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_mail(unittest.TestCase):
+class mail1(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_mail(self):
+    def test_mail1(self):
         success = True
         wd = self.wd
-        wd.get("https://mail.ru/")
+        wd.get("https://mail.ru/?from=logout")
         wd.find_element_by_id("mailbox:login").click()
         wd.find_element_by_id("mailbox:login").clear()
         wd.find_element_by_id("mailbox:login").send_keys("olegsupertest")
@@ -27,18 +27,16 @@ class test_mail(unittest.TestCase):
         wd.find_element_by_id("mailbox:password").send_keys("kessbae17")
         wd.find_element_by_css_selector("input.o-control").click()
         wd.find_element_by_link_text("Написать письмо").click()
+        wd.find_element_by_xpath("//div[@class='compose-head']/div[3]/div[1]/div/div/div[2]/div/div/div").click()
         wd.find_element_by_css_selector("textarea.js-input.compose__labels__input").click()
         wd.find_element_by_css_selector("textarea.js-input.compose__labels__input").clear()
         wd.find_element_by_css_selector("textarea.js-input.compose__labels__input").send_keys("o")
         wd.find_element_by_css_selector("div.b-correspondent__text").click()
-        wd.find_element_by_xpath("//div[@class='compose-head']/div[3]/div[4]/div/div/div[2]/div").click()
         wd.find_element_by_name("Subject").click()
         wd.find_element_by_name("Subject").clear()
-        wd.find_element_by_name("Subject").send_keys("yo teset")
-        wd.switch_to_frame("toolkit-152406790649642composeEditor_ifr_name")
-        wd.find_element_by_xpath("//body").click()
-        wd.switch_to_default_content()
+        wd.find_element_by_name("Subject").send_keys("yoyo")
         wd.find_element_by_xpath("//div[@id='b-toolbar__right']//span[.='Отправить']").click()
+        wd.find_element_by_xpath("//div[@class='is-compose-empty_in']//button[.='Продолжить']").click()
         wd.find_element_by_id("PH_logoutLink").click()
         self.assertTrue(success)
     
