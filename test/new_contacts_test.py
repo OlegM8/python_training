fe9a84b808f15488dcd9531a5c6b7a3aae934ab5
Oleg <omikheenko@distillery.com>
2018-06-26 00:08:32 +0300

    26.06

--- test/new_contacts_test.py
+++ test/new_contacts_test.py
@@ -12,7 +12,7 @@ def test_info_on_homepage(app):
 def test_selected_list(app):
     info_from_select = app.contact.get_info_from_select(0)
     info_from_edit = app.contact.get_info_from_edit(0)
-#   assert info_from_select.info == clear(info_from_edit.info)
+#    assert info_from_select.info == clear(info_from_edit.info)
     assert info_from_select.f_name == clear(info_from_edit.f_name)
     assert info_from_select.l_name == clear(info_from_edit.l_name)
     assert info_from_select.company == clear(info_from_edit.company)
@@ -20,4 +20,4 @@ def test_selected_list(app):
     assert info_from_select.phone == clear(info_from_edit.phone)
 
 def clear(s):
-    return re.sub("[(+) -]", "", s)
+    return re.sub("[(+)-]", "", s)
