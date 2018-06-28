4479fa15cf5fec6f808a0fb0baaa2cf0a482d187
Oleg Mikheenko <olegmikheenko@Olegs-MacBook-Pro.local>
2018-06-28 17:22:16 +0300

    lesson 6_5

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(f_name="f_name1", l_name="l_name1", company="Company1"),
+    Contact(f_name="f_name2", l_name="l_name2", company="Company2")
+]
